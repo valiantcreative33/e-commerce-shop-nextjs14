ea22f0a3cfc42193ee7d1c5352134069d8886b03
valiantcreative33 <ruben@valiantcreative.net>
2024-01-05 00:02:10 -0500

    added root styling and started header setup

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import Link from 'next/link'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  return (
+    <nav>
+        <Gutter>
+            <Link href="/">
+                <img src="/logo-black.svg" alt="logo" />
+            </Link>
+        </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
