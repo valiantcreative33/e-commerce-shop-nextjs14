b5e410bce0ae961d70ec07c8c1de5408b97d93e5
valiantcreative33 <ruben@valiantcreative.net>
2024-01-09 21:24:39 -0500

    created header logic and styling

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 
 import { Header } from '../../../payload/payload-types'
 import { fetchHeader } from '../../_api/fetchGlobals'
-import HeaderComponent from './HeaderComponent';
+import HeaderComponent from './HeaderComponent'
 
 export async function Header() {
   let header: Header | null = null
@@ -15,10 +15,7 @@ export async function Header() {
   try {
     header = await fetchHeader()
   } catch (error) {
-    // When deploying this template on Payload Cloud, this page needs to build before the APIs are live
-    // So swallow the error here and simply render the header without nav items if one occurs
-    // in production you may want to redirect to a 404  page or at least log the error somewhere
-    console.error(error)
+    console.log(error)
   }
 
   return (
